c3956df15f5c970a8209bf1ee6db82f69ddbb645
User-2021SVQTSS\Administrator <16fekim@gmail.com>
2022-01-14 12:57:03 +0900

    palindromeNumber

--- src/_9PalindromeNumber/_1UsingString.java
+++ src/_9PalindromeNumber/_1UsingString.java
@@ -2,19 +2,30 @@ package _9PalindromeNumber;
 
 public class _1UsingString
 {
-    public boolean isPalindrome(int x)
+    /* String과 StringBuffer를 이용한 구현 */
+    public boolean isPalindrome1(int x)
     {
         String strOrigin = Integer.toString(x);
         StringBuffer temp = new StringBuffer(strOrigin);
         temp.reverse();
         String strCompare = temp.toString();
 
-        System.out.println(strOrigin);
-        System.out.println(strCompare);
+        return strOrigin.equals(strCompare);
+    }
 
-        if(strOrigin.equals(strCompare))
-            return true;
-        else
-            return false;
+    /* 나누기, 나머지 연산을 이용한 구현 */
+    public boolean isPalindrome2(int x)
+    {
+        int n = x;
+        int r;
+        int temp = 0;
+
+        while(n > 0)
+        {
+            r = n % 10;
+            temp = temp*10 + r;
+            n = n / 10;
+        }
+        return (x == temp);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,12 @@ public class Main
     public static void main(String[] args)
     {
         _1UsingString sol = new _1UsingString();
-        System.out.println(sol.isPalindrome(525));
+
+        long start = System.nanoTime();
+
+        System.out.println(sol.isPalindrome2(123321));
+
+        long end = System.nanoTime();
+        System.out.println("수행시간: " + (end - start) + " ns");
     }
 }

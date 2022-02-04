6b3a2cbe3ce135415158a7068e04c76c474b5b71
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-02-04 22:35:17 +0900

    solve array problems

--- src/Inflearn/_2Array/Main.java
+++ src/Inflearn/_2Array/Main.java
@@ -1,7 +1,9 @@
 package Inflearn._2Array;
 
+import java.io.SequenceInputStream;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -9,6 +11,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
+
 
     }
 
@@ -50,6 +53,102 @@ public class Main {
             answer[i] = answer[i-1] + answer[i-2];
         }
         return answer;
+    }*/
+    /* 5. 소수(에라토스테네스 체) */
+    /*public static int primeNumber(int n){
+        int answer = 0;
+        int[] ch = new int[n+1];
+        for(int i=2; i<=n; ++i){
+            if(ch[i] == 0) {
+                answer++;
+                for (int j = i; j <= n; j = j + i) {
+                    ch[j] = 1;
+                }
+            }
+        }
+        return answer;
+    }*/
+    /* 6. 뒤집은 소수 */
+    /*public static ArrayList<Integer> reversedPrimeNumber(int n, int[] nums){
+        ArrayList<Integer> answer = new ArrayList<>();
+        for(int i=0; i<nums.length; ++i){
+            int temp = nums[i];
+            int res =0; int t = 0;
+            while(temp > 0) {
+                t = temp % 10;
+                res = res*10 + t;
+                temp = temp / 10;
+            }
+            if(isPrime(res))
+                answer.add(res);
+        }
+        return answer;
+    }
+    public static boolean isPrime(int num){
+        if(num == 1)return false;
+        for(int i=2; i<num; ++i){
+            if(num%i==0) return false;
+        }
+        return true;
+    }*/
+    /* 7. 점수 계산 */
+    /*public static int calScore(int n, int[] scores){
+        int answer = 0;
+        int sequence = 0;
+        for(int i=0; i<scores.length; ++i) {
+            if(scores[i] == 0) sequence = 0;
+            else answer += (sequence++) + 1;
+        }
+        return answer;
+    }*/
+    /* 8. 등수 구하기 */
+    /*public static ArrayList<Integer> lineUp(int n, int[] scores){
+        ArrayList<Integer> rank = new ArrayList<>();
+
+        for(int i=0; i< scores.length; ++i){
+            int pos = 1;
+            for(int j=0; j<scores.length; ++j){
+                if(scores[i] < scores[j])
+                    pos ++;
+            }
+            rank.add(pos);
+        }
+        return rank;
+    }*/
+    /* 9. 격자판 최대합 */
+    /*public static int gridSum(int[][] arr){
+        int max = -1;
+        int sum1=0; int sum2=0; int sum3=0; int sum4 = 0;
+
+        for(int i=0; i<arr[0].length; ++i){
+            for(int j=0; j<arr[0].length; ++j){
+                sum1 += arr[i][j];
+                sum2 += arr[j][i];
+            }
+            max = Math.max(max, sum1);
+            max = Math.max(max, sum2);
+            sum2 = 0;
+            sum1 = 0;
+        }
+
+        for(int i=0; i<arr[0].length; ++i){
+            for(int j=0; j<arr[0].length; ++j){
+                if(i == j)
+                    sum3 += arr[j][i];
+            }
+        }
+        max = Math.max(max,sum3);
+        sum3 = 0;
+
+        for(int i=0; i<arr[0].length; ++i){
+            for(int j=0; j<arr[0].length; ++j){
+                if(i == (arr[0].length-j-1))
+                    sum4 += arr[j][i];
+            }
+        }
+        max = Math.max(max,sum4);
+
+        return max;
     }*/
 
 }

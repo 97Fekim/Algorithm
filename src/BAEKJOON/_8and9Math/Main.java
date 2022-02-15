1ea33c7e06c7fe9dbc06de6128ea67dfee186a77
User-2021SVQTSS\Administrator <16fekim@gmail.com>
2022-02-15 17:28:31 +0900

    solve math problem on boj

--- src/BAEKJOON/_8and9Math/Main.java
+++ src/BAEKJOON/_8and9Math/Main.java
@@ -1,2 +1,21 @@
-package BAEKJOON._8and9Math;public class Main {
+package BAEKJOON._8and9Math;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner kb = new Scanner(System.in);
+
+        int a = kb.nextInt();
+        int b = kb.nextInt();
+        int v = kb.nextInt();
+
+        float x = (float)(v-b)/(a-b);
+        System.out.println((int)Math.ceil(x));
+
+
+    }
+
 }
+

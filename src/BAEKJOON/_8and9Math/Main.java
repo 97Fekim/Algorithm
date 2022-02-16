8010cf73bae025731e40f7c3294d0c92a8b6bb80
User-2021SVQTSS\Administrator <16fekim@gmail.com>
2022-02-16 10:19:55 +0900

    solve BOJ Math problems

--- src/BAEKJOON/_8and9Math/Main.java
+++ src/BAEKJOON/_8and9Math/Main.java
@@ -4,8 +4,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
 
+        int n = kb.nextInt();
 
+        for(int i=2; i<=Math.sqrt(n); ++i){
+            while(n % i == 0){
+                System.out.println(i);
+                n /= i;
+            }
+        }
+
+        if(n != 1){
+            System.out.println(n);
+        }
     }
 }
 /* 2581 소수 */

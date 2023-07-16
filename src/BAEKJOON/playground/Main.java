661656a7517a095371349c1bdc0b15424b2e296c
Fekim <81150979+97Fekim@users.noreply.github.com>
2023-07-17 00:09:59 +0900

    playground

---
+++ src/BAEKJOON/playground/Main.java
@@ -0,0 +1,93 @@
+package BAEKJOON.playground;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int pointX = Integer.parseInt(br.readLine());
+        int pointY = Integer.parseInt(br.readLine());
+
+        int quadrant = 0;
+
+        if (pointX < 0 && pointY < 0) {
+            quadrant = 3;
+        } else if (pointX < 0 && pointY > 0) {
+            quadrant = 2;
+        } else if (pointX > 0 && pointY < 0) {
+            quadrant = 4;
+        } else {
+            quadrant = 1;
+        }
+
+        System.out.println(quadrant);
+
+    }
+
+/*    public static void main(String[] args) throws IOException {
+
+//        int a = Integer.parseInt(br.readLine());
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+//        st = new StringTokenizer(br.readLine(), " ");  // 정수배열만들기
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] bowl = new int[N];
+
+        for(int i=0; i<N; ++i) {
+            bowl[i] = i+1;
+        }
+
+        int left = 0;
+        int right = 0;
+        int temp = 0;
+
+        for(int i=0; i<M; ++i) {
+
+            st = new StringTokenizer(br.readLine(), " ");
+
+            left = Integer.parseInt(st.nextToken());
+            right = Integer.parseInt(st.nextToken());
+
+            while(left-1 < right-1) {
+
+                temp = bowl[left-1];
+                bowl[left-1] = bowl[right-1];
+                bowl[right-1] = temp;
+
+                left++;
+                right--;
+            }
+
+        }
+
+        for(int i=0; i<bowl.length; ++i) {
+            System.out.print(bowl[i] + " ");
+        }
+
+
+    }*/
+
+/*    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        System.out.println(Long.parseLong(st.nextToken()) + Long.parseLong(st.nextToken()) + Long.parseLong(st.nextToken()));
+
+    }*/
+
+}

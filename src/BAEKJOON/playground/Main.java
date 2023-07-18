960e7384154e96a46006a98546c733a802c11bdb
Fekim <81150979+97Fekim@users.noreply.github.com>
2023-07-18 23:24:01 +0900

    단계별로 풀기

--- src/BAEKJOON/playground/Main.java
+++ src/BAEKJOON/playground/Main.java
@@ -3,7 +3,6 @@ package BAEKJOON.playground;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,24 +11,159 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int pointX = Integer.parseInt(br.readLine());
-        int pointY = Integer.parseInt(br.readLine());
+        int maxI = 0;
+        int maxJ = 0;
+        int max = 0;
+        int curVal = 0;
 
-        int quadrant = 0;
+        for(int i=0; i<9; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        if (pointX < 0 && pointY < 0) {
-            quadrant = 3;
-        } else if (pointX < 0 && pointY > 0) {
-            quadrant = 2;
-        } else if (pointX > 0 && pointY < 0) {
-            quadrant = 4;
-        } else {
-            quadrant = 1;
+            for(int j=0; j<9; j++) {
+                curVal = Integer.parseInt(st.nextToken());
+                if(curVal > max) {
+                    max = curVal;
+                    maxI = i;
+                    maxJ = j;
+                }
+            }
         }
 
-        System.out.println(quadrant);
+        System.out.println(max);
+        System.out.print((maxI+1) + " " + (maxJ+1));
 
     }
+
+/*    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Map<String, Double> scoreMap = new HashMap<>();
+        scoreMap.put("A+",4.5);
+        scoreMap.put("A0",4.0);
+        scoreMap.put("B+",3.5);
+        scoreMap.put("B0",3.0);
+        scoreMap.put("C+",2.5);
+        scoreMap.put("C0",2.0);
+        scoreMap.put("D+",1.5);
+        scoreMap.put("D0",1.0);
+        scoreMap.put("F",0.0);
+
+
+        double totSum = 0.0d;
+        double totCnt = 0.0d;
+
+        String curGrade = "";
+        double curScore = 0.0d;
+
+        while(true) {
+
+            String inStr = br.readLine();
+            if(inStr == null || inStr.isEmpty()){
+                break;
+            }
+
+            StringTokenizer st = new StringTokenizer(inStr, " ");
+
+            String temp = st.nextToken(); // dummy
+            curScore = Double.parseDouble(st.nextToken());
+            curGrade = st.nextToken();
+
+            if(!curGrade.equals("P")) {
+
+                totCnt += curScore;
+                totSum += curScore * scoreMap.get(curGrade);
+
+            }
+
+        }
+
+        System.out.print(totSum / totCnt);
+
+
+    }*/
+
+/*    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int size = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<size; i+=4) {
+            System.out.print("long" + " ");
+        }
+
+        System.out.print("int");
+    }*/
+
+/*    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sumAmt = Integer.parseInt(br.readLine());
+        int totCnt = Integer.parseInt(br.readLine());
+        int actualAmt = 0;
+
+        for(int i=0; i<totCnt; ++i) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            actualAmt += Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(sumAmt == actualAmt ? "Yes" : "No");
+
+    }*/
+
+/*    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int first = Integer.parseInt(st.nextToken());
+        int second = Integer.parseInt(st.nextToken());
+        int third = Integer.parseInt(st.nextToken());
+        int max = first;
+
+        if (first == second && second == third) {
+            System.out.println(10000+first*1000);
+        } else if (first == second) {
+            System.out.println(1000 + 100*first);
+        } else if (first == third) {
+            System.out.println(1000 + 100*first);
+        } else if (second == third) {
+            System.out.println(1000 + 100*second);
+        } else {
+            if(second > first && second > third) {
+                max = second;
+            } else if (third > first && third > second) {
+                max = third;
+            }
+
+            System.out.println(100*max);
+        }
+
+    }*/
+
+//    public static void main(String[] args) throws IOException {
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int pointX = Integer.parseInt(br.readLine());
+//        int pointY = Integer.parseInt(br.readLine());
+//
+//        int quadrant = 0;
+//
+//        if (pointX < 0 && pointY < 0) {
+//            quadrant = 3;
+//        } else if (pointX < 0 && pointY > 0) {
+//            quadrant = 2;
+//        } else if (pointX > 0 && pointY < 0) {
+//            quadrant = 4;
+//        } else {
+//            quadrant = 1;
+//        }
+//
+//        System.out.println(quadrant);
+//
+//    }
 
 /*    public static void main(String[] args) throws IOException {
 

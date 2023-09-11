51cc90b115a8e87d5f739c5e3f6d358d87e0fdbb
97Fekim <rlatmdcjf9@gmail.com>
2023-09-11 16:06:46 +0900

    LCS

--- src/BAEKJOON/playground/Main.java
+++ src/BAEKJOON/playground/Main.java
@@ -1,12 +1,45 @@
 package BAEKJOON.playground;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
 
-    // #10942 펠린드롬?
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] str1 = br.readLine().toCharArray();
+        char[] str2 = br.readLine().toCharArray();
+        int[][] dp = new int[str1.length+1][str2.length+1];
+        StringBuilder[][] answer = new StringBuilder[str1.length+1][str2.length+1];
+
+        for (int i=0; i< answer.length; ++i) {
+            Arrays.fill(answer[i], new StringBuilder(""));
+        }
+
+        for (int i=1; i<=str1.length; ++i) {
+            for (int j=1; j<=str2.length; ++j) {
+                if (str1[i-1] == str2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                    answer[i][j] = answer[i-1][j-1].append(str2[j-1]);
+                } else {
+                    if (dp[i][j-1] > dp[i-1][j]) {
+                        dp[i][j] = dp[i][j-1];
+                        answer[i][j] = answer[i][j-1];
+                    } else {
+                        dp[i][j] = dp[i-1][j];
+                        answer[i][j] = answer[i-1][j];
+                    }
+                }
+            }
+        }
+
+        System.out.println(dp[str1.length][str2.length]);
+        System.out.println(answer[str1.length][str2.length]);
+
+    }
+
+    // #10942 펠린드롬?
+/*    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
@@ -56,13 +89,7 @@ public class Main {
         bw.flush();
         bw.close();
 
-//        for (int i=1; i<=N; ++i) {
-//            for (int j=1; j<=N; ++j) {
-//                System.out.print(dp[i][j] ? 'O' + " " : 'X' + " ");
-//            }
-//            System.out.println();
-//        }
-    }
+    }*/
 
     // 스도쿠
 /*    static int[][] arr;

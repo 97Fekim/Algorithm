8dfda0a66ee4e831373d6e21488ef9dc8ed2ef23
Fekim <81150979+97Fekim@users.noreply.github.com>
2023-10-31 23:52:15 +0900

    그리디

--- src/BAEKJOON/playground/Main.java
+++ src/BAEKJOON/playground/Main.java
@@ -1,11 +1,28 @@
 package BAEKJOON.playground;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long answer = 0L;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i=0; i<N; ++i) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i=0; i<N; ++i) {
+            answer += Math.abs(arr[i] - (i+1));
+        }
+
+        System.out.println(answer);
 
     }
 
